65efa456510f818bc087902d01d445d4da1182e0
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-12-05 11:44:51 +0100

    Add files via upload

---
+++ hello.s
@@ -0,0 +1,21 @@
+/* This source assembler file can generate an executable file with ‘makefile_hello’ file. */
+
+    .data
+msg :    .asciz "Salut tout le monde !\n"
+len = . - msg
+.bss
+.text
+.global _start  
+     
+_start :
+movl $msg,%ecx
+movl $len,%edx
+movl $1,%ebx
+movl $4,%eax
+int  $0x80           # appel système
+     
+exit :  
+movl $0,%ebx  /* Ces instructions permettent de terminer le code */
+movl $1,%eax
+int  $0x80
+
